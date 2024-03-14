520e408e2d87f993326309f556608d658bbe04ff
Harsh Bansal <hbnsl890@gmail.com>
2024-03-15 04:14:07 +0530

    Update: basic functionalities of context methods     are added, addtodo, updatetodo, deletetodo,     togglecomplete, etc. Revise: maps, filters, spread of JS Usage: used setTodo state to access all the todos        ,and defin etheir context functions

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,41 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { ToDoProvider } from './context/ToDoContext'
+
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [todos, setTodos] = useState([])
+
+  const addTodo = (todo) => {
+      setTodos((prevToDo)=> [{id: Date.now(), ...todo}, ...prev])
+  }
+  const updateTodo = (id, todo) =>{
+    setTodos((prev)=> prev.map( (prevToDo) => prevToDo.id === id? todo: prevToDo) );
+  }
+  const deleteTodo = () =>{
+    setTodos((prev)=>{
+      prev.filter((todo)=> todo.id!= id)
+    })
+  }
+  const toggleComplete = (id)=>{
+    setTodos((prev)=> prev.map(
+      (prevtodo)=> prevtodo.id === id? {...prevtodo, completed: !prevtodo.completed}:prevtodo
+    ))
+  }
 
   return (
-    <>
-      
-    </>
+    <ToDoProvider value={{todos, addTodo, updateTodo, deleteTodo, toggleComplete}} >
+      <div className="bg-[#172842] min-h-screen py-8">
+        <div className="w-full max-w-2xl mx-auto shadow-md rounded-lg px-4 py-3 text-white">
+          <h1 className="text-2xl font-bold text-center mb-8 mt-2">Manage Your Todos</h1>
+          <div className="mb-4">
+            {/* Todo form goes here */}
+          </div>
+          <div className="flex flex-wrap gap-y-3">
+            {/*Loop and Add TodoItem here */}
+          </div>
+        </div>
+      </div>
+    </ToDoProvider>
   )
 }
 
